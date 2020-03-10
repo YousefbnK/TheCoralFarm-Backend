97a8d534cfa8346d188d5b302a12275fafc34fc1
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-10 17:08:24 +0300

    Login APIview complete

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -19,17 +20,22 @@ class UserCreateSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    access = serializers.CharField(allow_blank=True, read_only=True)
 
     def validate(self, data):
         username = data.get('username')
         password = data.get('password')
 
         try:
-            user_obj = User.object.get(username=username)
+            user_obj = User.objects.get(username=username)
         except:
             raise serializers.ValidationError("This username does not exist")
 
         if not user_obj.check_password(password):
-            raise serializers.ValidationError("Incorrect username or password")
+            raise serializers.ValidationError("Incorrect password")
+
+        payload = RefreshToken.for_user(user_obj)
+        token = str(payload.access_token)
+        data["access"] = token
 
         return data
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserCreateAPIView
+from .views import UserCreateAPIView, UserLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view() , name='login'),
+    path('login/', UserLoginView.as_view() , name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
 ]
